dbbb3f3a521a208b843c279ffd4cae8f295e59da
UttkarshRithe <yashrithe38@gmail.com>
2026-05-18 11:56:55 +0530

    fix: enable feign clients and web security in auth-service and evaluation-service

--- Backend/auth-service/src/main/java/com/techtraining/auth/AuthServiceApplication.java
+++ Backend/auth-service/src/main/java/com/techtraining/auth/AuthServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(
         scanBasePackages = {
@@ -20,6 +21,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 )
 
 @EnableDiscoveryClient
+@EnableFeignClients
 public class AuthServiceApplication {
 
     public static void main(String[] args) {
